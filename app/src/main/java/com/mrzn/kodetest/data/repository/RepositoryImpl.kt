4cbf8d291b94ab769e698029f2e45bed2eaab832
Andrey Murzin <murzzzin_2010@mail.ru>
2025-12-14 17:28:10 +0800

    feat(data): add Repository implementation

---
+++ app/src/main/java/com/mrzn/kodetest/data/repository/RepositoryImpl.kt
@@ -0,0 +1,61 @@
+package com.mrzn.kodetest.data.repository
+
+import com.mrzn.kodetest.data.mapper.EmployeesMapper
+import com.mrzn.kodetest.data.network.api.ApiService
+import com.mrzn.kodetest.domain.entity.Employee
+import com.mrzn.kodetest.domain.repository.Repository
+import com.mrzn.kodetest.domain.result.LoadResult
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class RepositoryImpl @Inject constructor(
+    private val apiService: ApiService,
+    private val mapper: EmployeesMapper
+) : Repository {
+
+    private val scope = CoroutineScope(Dispatchers.Default)
+
+    private val _employees = mutableListOf<Employee>()
+    private val employees: List<Employee>
+        get() = _employees.toList()
+
+    private val loadEmployeesEvents = MutableSharedFlow<Unit>(replay = 1)
+
+    private val loadEmployees: StateFlow<LoadResult> = flow {
+        loadEmployeesEvents.emit(Unit)
+        loadEmployeesEvents.collect {
+            val response = apiService.loadEmployees()
+            _employees.addAll(mapper.mapResponseToEmployees(response))
+            emit(employees)
+        }
+    }
+        .map { LoadResult.Success(it) as LoadResult }
+        .catch {
+            emit(
+                when (it) {
+                    is HttpException -> LoadResult.Failure.ServerError
+                    else -> LoadResult.Failure.NoInternet
+                }
+            )
+        }
+        .stateIn(
+            scope = scope,
+            started = SharingStarted.Lazily,
+            initialValue = LoadResult.Success(employees)
+        )
+
+    override fun getEmployees(): StateFlow<LoadResult> = loadEmployees
+
+    override suspend fun refreshEmployees() {
+        loadEmployeesEvents.emit(Unit)
+    }
+}
