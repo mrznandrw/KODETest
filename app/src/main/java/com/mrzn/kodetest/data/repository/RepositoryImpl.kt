86e04cd9e67beabb23907f8b2134f66613eb8286
Andrey Murzin <murzzzin_2010@mail.ru>
2025-12-19 19:49:55 +0800

    fix(data): reload employees correctly after no internet state

--- app/src/main/java/com/mrzn/kodetest/data/repository/RepositoryImpl.kt
+++ app/src/main/java/com/mrzn/kodetest/data/repository/RepositoryImpl.kt
@@ -2,16 +2,18 @@ package com.mrzn.kodetest.data.repository
 
 import com.mrzn.kodetest.data.mapper.EmployeesMapper
 import com.mrzn.kodetest.data.network.api.ApiService
-import com.mrzn.kodetest.domain.entity.Employee
 import com.mrzn.kodetest.domain.repository.Repository
 import com.mrzn.kodetest.domain.result.LoadResult
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import retrofit2.HttpException
 import javax.inject.Inject
@@ -23,32 +25,33 @@ class RepositoryImpl @Inject constructor(
 
     private val scope = CoroutineScope(Dispatchers.Default)
 
-    private val _employees = mutableListOf<Employee>()
-    private val employees: List<Employee>
-        get() = _employees.toList()
+    private val loadEmployeesEvents = MutableSharedFlow<Unit>()
 
-    private val loadEmployeesEvents = MutableSharedFlow<Unit>(replay = 1)
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val loadEmployees: StateFlow<LoadResult> =
+        loadEmployeesEvents
+            .onStart { emit(Unit) }
+            .flatMapLatest {
+                flow {
+                    emit(LoadResult.Loading)
 
-    private val loadEmployees: StateFlow<LoadResult> = flow {
-        loadEmployeesEvents.emit(Unit)
-        loadEmployeesEvents.collect {
-            emit(LoadResult.Loading)
-            val response = apiService.loadEmployees()
-            _employees.addAll(mapper.mapResponseToEmployees(response))
-            emit(LoadResult.Success(employees))
-        }
-    }.catch {
-        emit(
-            when (it) {
-                is HttpException -> LoadResult.Failure.ServerError
-                else -> LoadResult.Failure.NoInternet
-            }
-        )
-    }.stateIn(
-        scope = scope,
-        started = SharingStarted.Lazily,
-        initialValue = LoadResult.Initial
-    )
+                    val response = apiService.loadEmployees()
+                    val employees = mapper.mapResponseToEmployees(response)
+
+                    emit(LoadResult.Success(employees))
+                }.catch {
+                    emit(
+                        when (it) {
+                            is HttpException -> LoadResult.Failure.ServerError
+                            else -> LoadResult.Failure.NoInternet
+                        }
+                    )
+                }
+            }.stateIn(
+                scope = scope,
+                started = SharingStarted.Lazily,
+                initialValue = LoadResult.Initial
+            )
 
     override fun getEmployees(): StateFlow<LoadResult> = loadEmployees
 
