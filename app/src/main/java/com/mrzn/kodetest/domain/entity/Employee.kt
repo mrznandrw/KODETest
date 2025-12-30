06d17b235b913097fb3b9f0c8bfbb91612057836
Andrey Murzin <murzzzin_2010@mail.ru>
2025-12-30 16:41:24 +0800

    perf(domain): mark Employee as immutable to avoid extra recompositions

--- app/src/main/java/com/mrzn/kodetest/domain/entity/Employee.kt
+++ app/src/main/java/com/mrzn/kodetest/domain/entity/Employee.kt
@@ -1,7 +1,9 @@
 package com.mrzn.kodetest.domain.entity
 
+import androidx.compose.runtime.Immutable
 import java.time.LocalDate
 
+@Immutable
 data class Employee(
     val id: String,
     val avatarUrl: String,
