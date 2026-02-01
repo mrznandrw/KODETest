5ead9e90fe421146cb302c8c257a951fb3b8a8c1
Andrey Murzin <murzzzin_2010@mail.ru>
2026-02-01 21:48:31 +0800

    feat(presentation): add sorting list functionality to MainViewModel

--- app/src/main/java/com/mrzn/kodetest/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/mrzn/kodetest/presentation/main/MainViewModel.kt
@@ -2,18 +2,20 @@ package com.mrzn.kodetest.presentation.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.mrzn.kodetest.domain.entity.Employee
 import com.mrzn.kodetest.domain.result.LoadResult
 import com.mrzn.kodetest.domain.usecase.GetEmployeesUseCase
 import com.mrzn.kodetest.domain.usecase.RefreshEmployeesUseCase
-import com.mrzn.kodetest.extensions.mergeWith
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import java.time.LocalDate
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
@@ -21,13 +23,21 @@ class MainViewModel @Inject constructor(
     private val refreshEmployeesUseCase: RefreshEmployeesUseCase
 ) : ViewModel() {
 
+    private var currentSorting: SortType = SortType.ALPHABETICAL
+    private val loadedEmployees = mutableListOf<Employee>()
+
+    private val sortListEvents = MutableSharedFlow<Unit>()
+    private val sortListFlow: Flow<MainScreenState> = sortListEvents.map {
+        (screenState.value as MainScreenState.Employees).copy(
+            employees = loadedEmployees.sort(currentSorting),
+            sortType = currentSorting
+        )
+    }
+
     private val refreshListEvents = MutableSharedFlow<Unit>()
-
     private val refreshListFlow: Flow<MainScreenState> = refreshListEvents.map {
-        val currentState = screenState.value
-
-        if (currentState is MainScreenState.Employees) {
-            currentState.copy(isRefreshing = true)
+        if (loadedEmployees.isNotEmpty()) {
+            (screenState.value as MainScreenState.Employees).copy(isRefreshing = true)
         } else {
             MainScreenState.Loading
         }
@@ -35,21 +45,38 @@ class MainViewModel @Inject constructor(
 
     val loadResultFlow: Flow<MainScreenState> = getEmployeesUseCase().map {
         when (it) {
-            is LoadResult.Success -> MainScreenState.Employees(employees = it.employees)
+            is LoadResult.Success -> {
+                loadedEmployees.clear()
+                loadedEmployees.addAll(it.employees)
+                MainScreenState.Employees(
+                    employees = it.employees.sort(currentSorting),
+                    sortType = currentSorting
+                )
+            }
+
             LoadResult.Failure.NoInternet, LoadResult.Failure.ServerError -> {
-                val currentState = screenState.value
-                if (currentState is MainScreenState.Employees){
-                    currentState.copy(isRefreshing = false)
-                }else{
+                if (loadedEmployees.isNotEmpty()) {
+                    (screenState.value as MainScreenState.Employees).copy(isRefreshing = false)
+                } else {
                     MainScreenState.Error
                 }
             }
         }
     }
 
-    val screenState: StateFlow<MainScreenState> = loadResultFlow
+    fun changeSorting(sortType: SortType) {
+        currentSorting = sortType
+        viewModelScope.launch {
+            sortListEvents.emit(Unit)
+        }
+    }
+
+    val screenState: StateFlow<MainScreenState> = merge(
+        loadResultFlow,
+        refreshListFlow,
+        sortListFlow
+    )
         .onStart { emit(MainScreenState.Loading) }
-        .mergeWith(refreshListFlow)
         .stateIn(
             scope = viewModelScope,
             started = SharingStarted.Lazily,
@@ -60,6 +87,17 @@ class MainViewModel @Inject constructor(
         viewModelScope.launch {
             refreshListEvents.emit(Unit)
             refreshEmployeesUseCase()
+        }
+    }
+
+    private fun List<Employee>.sort(sortType: SortType) = when (sortType) {
+        SortType.ALPHABETICAL -> sortedBy { it.fullName } to emptyList()
+        SortType.BIRTHDAY -> {
+            sortedBy {
+                it.birthday.dayOfYear
+            }.partition {
+                it.birthday.dayOfYear >= LocalDate.now().dayOfYear
+            }
         }
     }
 }
