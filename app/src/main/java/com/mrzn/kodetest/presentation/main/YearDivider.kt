46e0084532ac4ce0e6c055b7ce43209834ada610
Andrey Murzin <murzzzin_2010@mail.ru>
2026-02-01 21:53:48 +0800

    feat(presentation): add YearDivider

---
+++ app/src/main/java/com/mrzn/kodetest/presentation/main/YearDivider.kt
@@ -0,0 +1,43 @@
+package com.mrzn.kodetest.presentation.main
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.time.LocalDate
+
+@Composable
+fun YearDivider() {
+    Row(
+        modifier = Modifier
+            .padding(vertical = 24.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        ConfiguredDivider()
+        Text(
+            text = "${LocalDate.now().year + 1}",
+            fontSize = 15.sp,
+            color = MaterialTheme.colorScheme.onSecondaryContainer
+        )
+        ConfiguredDivider()
+    }
+}
+
+@Composable
+fun ConfiguredDivider() {
+    HorizontalDivider(
+        modifier = Modifier.width(72.dp),
+        color = MaterialTheme.colorScheme.onSecondaryContainer
+    )
+}
