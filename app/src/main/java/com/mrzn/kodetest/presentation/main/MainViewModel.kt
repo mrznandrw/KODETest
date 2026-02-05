6d11074ff3055daacfe0f901bed45141e3a906c8
Andrey Murzin <murzzzin_2010@mail.ru>
2026-02-05 16:31:19 +0800

    feat(presentation): add search functionality to MainViewModel

--- app/src/main/java/com/mrzn/kodetest/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/mrzn/kodetest/presentation/main/MainViewModel.kt
@@ -1,5 +1,8 @@
 package com.mrzn.kodetest.presentation.main
 
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.clearText
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mrzn.kodetest.domain.entity.Employee
@@ -7,11 +10,13 @@ import com.mrzn.kodetest.domain.result.LoadResult
 import com.mrzn.kodetest.domain.usecase.GetEmployeesUseCase
 import com.mrzn.kodetest.domain.usecase.RefreshEmployeesUseCase
 import com.mrzn.kodetest.extensions.mergeWith
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
@@ -21,6 +26,7 @@ import kotlinx.coroutines.launch
 import java.time.LocalDate
 import javax.inject.Inject
 
+@OptIn(FlowPreview::class)
 class MainViewModel @Inject constructor(
     private val getEmployeesUseCase: GetEmployeesUseCase,
     private val refreshEmployeesUseCase: RefreshEmployeesUseCase
@@ -29,6 +35,9 @@ class MainViewModel @Inject constructor(
     private var employees = MutableStateFlow(emptyList<Employee>())
     private val isRefreshing = MutableStateFlow(false)
     private var currentSorting = MutableStateFlow(SortType.ALPHABETICAL)
+
+    private val searchQuery = TextFieldState()
+    private val searchFlow = snapshotFlow { searchQuery.text.trim() }.debounce(500)
 
     private val errorEvents = MutableSharedFlow<Unit>()
     private val errorFlow = errorEvents.map {
@@ -60,6 +69,15 @@ class MainViewModel @Inject constructor(
 
     private val sortedEmployees = employees
         .filter { it.isNotEmpty() }
+        .combine(searchFlow) { employees, query ->
+            if (query.isBlank()) {
+                employees
+            } else {
+                employees.filter {
+                    it.doesMatchSearchQuery(query.toString())
+                }
+            }
+        }
         .combine(currentSorting) { employees, sorting ->
             employees.sort(sorting)
         }
@@ -68,7 +86,8 @@ class MainViewModel @Inject constructor(
         .map {
             MainScreenState.Employees(
                 employees = it,
-                sortType = currentSorting.value
+                sortType = currentSorting.value,
+                searchQuery = searchQuery
             )
         }
         .onEach { isRefreshing.value = false }
@@ -99,6 +118,10 @@ class MainViewModel @Inject constructor(
 
     fun changeSorting(sortType: SortType) {
         currentSorting.value = sortType
+    }
+
+    fun clearSearch() {
+        searchQuery.clearText()
     }
 
     private fun List<Employee>.sort(sortType: SortType) = when (sortType) {
