f45ef22bc0a39f62dcf385c557942595fadb89af
Andrey Murzin <murzzzin_2010@mail.ru>
2026-02-09 22:39:10 +0800

    feat(presentation): implement navigation

--- app/src/main/java/com/mrzn/kodetest/presentation/MainActivity.kt
+++ app/src/main/java/com/mrzn/kodetest/presentation/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.mrzn.kodetest.presentation.main.MainScreen
+import com.mrzn.kodetest.navigation.NavigationRoot
 import com.mrzn.kodetest.presentation.ui.theme.KODETestTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KODETestTheme {
-                MainScreen()
+                NavigationRoot()
             }
         }
     }
