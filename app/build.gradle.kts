aaf51844fe4f16b311c1aa7c2063d83b2b63b09c
Andrey Murzin <murzzzin_2010@mail.ru>
2026-02-09 18:53:24 +0800

    build: add dependencies for Navigation 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,4 +79,8 @@ dependencies {
     implementation(libs.coil.network)
 
     coreLibraryDesugaring(libs.desugar.jdk.libs)
+
+    implementation(libs.androidx.navigation3.ui)
+    implementation(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.lifecycle.viewmodel.navigation3)
 }
