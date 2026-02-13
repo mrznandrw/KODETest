f7e35d10b2f7726eab1da9f7b85b445c6df5598f
Andrey Murzin <murzzzin_2010@mail.ru>
2026-02-13 20:59:08 +0800

    build: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,5 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ksp)
