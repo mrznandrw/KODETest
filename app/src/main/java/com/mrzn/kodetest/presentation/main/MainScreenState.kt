bb1d6d1ecd8f824ed3385b2f8b3a4b24f42209ab
Andrey Murzin <murzzzin_2010@mail.ru>
2026-02-13 13:38:10 +0800

    feat(presentation): add error to MainScreenState

--- app/src/main/java/com/mrzn/kodetest/presentation/main/MainScreenState.kt
+++ app/src/main/java/com/mrzn/kodetest/presentation/main/MainScreenState.kt
@@ -2,6 +2,7 @@ package com.mrzn.kodetest.presentation.main
 
 import androidx.compose.foundation.text.input.TextFieldState
 import com.mrzn.kodetest.domain.entity.Department
+import com.mrzn.kodetest.domain.result.LoadResult
 
 sealed class MainScreenState {
 
@@ -11,7 +12,8 @@ sealed class MainScreenState {
         val employees: Map<Department?, List<EmployeeListItem>>,
         val sortType: SortType,
         val searchQuery: TextFieldState,
-        val isRefreshing: Boolean = false
+        val isRefreshing: Boolean = false,
+        val error: LoadResult.Failure? = null
     ) : MainScreenState()
 
     data object Error : MainScreenState()
